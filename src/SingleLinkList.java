e1ad0bcdda25b4f1e9e26317f1f123c6a85f5733
Mateusz Ostróżka <mateusz.ostrozka34@gmail.com>
2025-11-18 14:20:27 +0100

    Node

---
+++ src/SingleLinkList.java
@@ -0,0 +1,46 @@
+public class SingleLinkList {
+    Node head;
+
+    SingleLinkList(){
+        head = null;
+    }
+
+    public void addFront(int data){
+        Node newNode = new Node(data,head);
+        head = newNode;
+    }
+
+    public void addBack(int data){
+        Node newNode = new Node(data,null);
+        Node temp = head;
+
+        if (head == null){
+            head = newNode;
+            return;
+        }
+
+        while(temp.next != null){
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    public void removeFront(){
+        if (head == null){
+            return;
+        }
+        if (head.next == null){
+            head = null;
+            return;
+        }
+
+        Node nodePointer = head;
+
+        while (nodePointer.next.next != null){
+            nodePointer = nodePointer.next;
+        }
+
+        nodePointer.next = null;
+    }
+}
